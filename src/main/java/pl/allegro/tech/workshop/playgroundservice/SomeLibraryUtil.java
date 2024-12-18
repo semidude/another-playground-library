fe20fb4bebe0bdc943e7da06fdc8c646709ed8ef
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2024-12-18 10:28:54 +0100

    Update log to 0.1.1

--- src/main/java/pl/allegro/tech/workshop/playgroundservice/SomeLibraryUtil.java
+++ src/main/java/pl/allegro/tech/workshop/playgroundservice/SomeLibraryUtil.java
@@ -3,6 +3,6 @@ package pl.allegro.tech.workshop.playgroundservice;
 public class SomeLibraryUtil {
 
     public void sayHello() {
-        System.out.println("Hello from library util");
+        System.out.println("Hello from library util 0.1.1");
     }
 }
