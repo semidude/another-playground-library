6cffe85ffbb41061f79d30a829b89e7389baa4d0
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2024-12-18 10:40:55 +0100

    Update log to 0.1.2-RC2

--- src/main/java/pl/allegro/tech/workshop/playgroundservice/SomeLibraryUtil.java
+++ src/main/java/pl/allegro/tech/workshop/playgroundservice/SomeLibraryUtil.java
@@ -3,6 +3,6 @@ package pl.allegro.tech.workshop.playgroundservice;
 public class SomeLibraryUtil {
 
     public void sayHello() {
-        System.out.println("Hello from library util 0.1.2-RC1");
+        System.out.println("Hello from library util 0.1.2-RC2");
     }
 }
