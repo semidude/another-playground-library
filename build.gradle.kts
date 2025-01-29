b3274418209f30c5d93faf56b21265a203a9be6c
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2025-01-29 21:06:05 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("pl.allegro.tech.build.axion-release") version "1.18.16"
+}
+
+scmVersion {
+    releaseOnlyOnReleaseBranches = true
+    unshallowRepoOnCI = true
+}
+
+group = "dev.panuszewski"
+version = scmVersion.version
+
+publishing {
+    publications {
+        create<MavenPublication>("library") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "githubPackages"
+            url = uri("https://maven.pkg.github.com/semidude/playground-library")
+            credentials {
+                username = "random-username"
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
---
+++ src/main/java/dev/panuszewski/SomeLibraryUtil.java
@@ -0,0 +1,8 @@
+package dev.panuszewski;
+
+public class SomeLibraryUtil {
+
+    public void sayHello() {
+        System.out.println("Hello from library util 0.1.2");
+    }
+}
